c0760ae0e36925c9de9076389c0edf3903d44952
Pavel Kucharenka <pkucharenka@gmail.com>
2016-03-19 18:08:11 +0300

    new

--- Turtle/Turtle/Program.cs
+++ Turtle/Turtle/Program.cs
@@ -1,15 +1,35 @@
-﻿using System;
+﻿using Microsoft.SmallBasic.Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Turtle
+namespace Turtle1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Turtle.Speed = 5;
+            Turtle.PenUp();
+            Turtle.MoveTo(100, 80);
+            Turtle.PenDown();
+            Turtle.MoveTo(200, 80);
+            Turtle.MoveTo(200, 180);
+            Turtle.MoveTo(100, 180);
+            Turtle.MoveTo(100, 80);
+            Turtle.MoveTo(150, 10);
+            Turtle.MoveTo(200, 80);
+            Turtle.PenUp();
+            Turtle.MoveTo(130, 100);
+            Turtle.PenDown();
+            Turtle.MoveTo(170, 100);
+            Turtle.MoveTo(170, 150);
+            Turtle.MoveTo(130, 150);
+            Turtle.MoveTo(130, 100);
+            Turtle.PenUp();
+            Turtle.MoveTo(0, 0);
         }
     }
 }
